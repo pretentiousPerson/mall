fe65c92d673d3d3d3e985ad4d210e2a77f5ee1c3
Administrator <Administrator@USER-20170110BU>
2018-05-29 18:31:05 +0800

    经过两天的调试，终于搭建好SSM框架、配置好log4j、并测试成功用户登录接口。

--- src/main/java/com/baijia/mall/dao/UserMapper.java
+++ src/main/java/com/baijia/mall/dao/UserMapper.java
@@ -1,5 +1,6 @@
 package com.baijia.mall.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.baijia.mall.pojo.User;
@@ -14,8 +15,12 @@ public interface UserMapper {
     int insertSelective(User record);
 
     User selectByPrimaryKey(Integer id);
+    
+    User selectLogin(@Param("username")String username,@Param("password")String password);
 
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+    
+    int checkUsername(String username);
 }
