e50a6425a4c46165e5366424408c31f11a5b4e56
pretentiousPerson <837579301@qq.com>
2018-05-31 16:32:56 +0800

    用户模块完结，待测试。

--- src/main/java/com/baijia/mall/dao/UserMapper.java
+++ src/main/java/com/baijia/mall/dao/UserMapper.java
@@ -16,11 +16,34 @@ public interface UserMapper {
 
     User selectByPrimaryKey(Integer id);
     
-    User selectLogin(@Param("username")String username,@Param("password")String password);
+    User selectLogin(
+    		@Param("username")String username,
+    		@Param("password")String password);
 
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
     
     int checkUsername(String username);
+    
+    int checkEmail(String email);
+    
+    String selectQuestionByUsername(String username);
+    
+    int checkAnswer(
+    		@Param("username")String username,
+    		@Param("question")String question,
+    		@Param("answer")String answer);
+    
+    int updatePasswordByUsername(@Param("username")
+    String username, @Param("passwordNew")
+    String passwordNew);
+
+    int checkPassword(@Param(value = "password")
+    String password, @Param("userId")
+    Integer userId);
+
+    int checkEmailByUserId(@Param(value = "email")
+    String email, @Param(value = "userId")
+    Integer userId);
 }
