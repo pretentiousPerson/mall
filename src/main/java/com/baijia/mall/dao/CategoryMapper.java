3865fdc1f9de0def846650a6e057178f13d8e448
pretentiousPerson <837579301@qq.com>
2018-06-02 16:58:04 +0800

    后台品类管理

--- src/main/java/com/baijia/mall/dao/CategoryMapper.java
+++ src/main/java/com/baijia/mall/dao/CategoryMapper.java
@@ -1,10 +1,9 @@
 package com.baijia.mall.dao;
 
-import org.springframework.stereotype.Repository;
-
 import com.baijia.mall.pojo.Category;
 
-@Repository
+import java.util.List;
+
 public interface CategoryMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -17,4 +16,6 @@ public interface CategoryMapper {
     int updateByPrimaryKeySelective(Category record);
 
     int updateByPrimaryKey(Category record);
+
+    List<Category> selectCategoryChildrenByParentId(Integer parentId);
 }
