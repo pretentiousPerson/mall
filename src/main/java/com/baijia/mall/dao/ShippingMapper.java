fee0ef73f883f53b921b373111a9d987163c7a1a
pretentiousPerson <837579301@qq.com>
2018-06-03 17:38:46 +0800

    完成管理员管理分类和收货地址管理两个模块

--- src/main/java/com/baijia/mall/dao/ShippingMapper.java
+++ src/main/java/com/baijia/mall/dao/ShippingMapper.java
@@ -1,5 +1,8 @@
 package com.baijia.mall.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.baijia.mall.pojo.Shipping;
@@ -17,4 +20,14 @@ public interface ShippingMapper {
     int updateByPrimaryKeySelective(Shipping record);
 
     int updateByPrimaryKey(Shipping record);
+    
+    int deleteByShippingIdUserId(@Param("userId")Integer userId,@Param("shippingId") Integer shippingId);
+
+    int updateByShipping(Shipping record);
+    
+    Shipping selectByShippingIdUserId(@Param("userId")Integer userId,@Param("shippingId") Integer shippingId);
+
+    List<Shipping> selectByUserId(@Param("userId")Integer userId);
+    
+    
 }
